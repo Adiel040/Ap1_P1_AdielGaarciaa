dd33ec6d0c04393398f06979a483d080c3321547
Adiel040 <133602314+Adiel040@users.noreply.github.com>
2024-06-03 18:31:32 -0400

    Creando el modelo articulos

---
+++ Ap1_P1_AdielGaarciaa/Models/Articulos.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ap1_P1_AdielGaarciaa.Models
+{
+    public class Articulos
+    {
+
+        //Articulo id, Descripcion , costo , ganancia , precio
+
+        [Key]
+        public int ArticuloId { get; set; }
+        [Required(ErrorMessage = "El campo Descripci&oacute;n es obligatorio ")]
+        public string? Descripcion { get; set; }
+        public decimal costo { get; set; }
+        public decimal ganancia { get; set; }
+        public decimal precio { get; set; }
+
+
+
+
+    }
+}
