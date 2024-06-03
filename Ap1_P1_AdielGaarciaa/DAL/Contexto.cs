75e020aaa879dbfbab7caba78c3fd70b7b396393
Adiel040 <133602314+Adiel040@users.noreply.github.com>
2024-06-03 18:40:44 -0400

    Creando el contexto

---
+++ Ap1_P1_AdielGaarciaa/DAL/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using Microsoft.EntityFrameworkCore;
+using Ap1_P1_AdielGaarciaa.Models;
+namespace Ap1_P1_AdielGaarciaa.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options)
+            : base(options) { }
+
+        public DbSet<Articulos> Articulos {get; set;}
+    }
+}
