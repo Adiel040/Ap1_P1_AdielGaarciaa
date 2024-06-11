bb4ac3bef51bd238862a02ac02872ec1fadc5325
Adiel040 <133602314+Adiel040@users.noreply.github.com>
2024-06-11 18:05:57 -0400

    inyectando el services y demas

--- Ap1_P1_AdielGaarciaa/Program.cs
+++ Ap1_P1_AdielGaarciaa/Program.cs
@@ -1,4 +1,8 @@
 using Ap1_P1_AdielGaarciaa.Components;
+using Ap1_P1_AdielGaarciaa.DAL;
+using Ap1_P1_AdielGaarciaa.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace Ap1_P1_AdielGaarciaa
 {
@@ -14,6 +18,10 @@ namespace Ap1_P1_AdielGaarciaa
             // Add services to the container.
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
+
+            var ConStr = builder.Configuration.GetConnectionString("ConStr");
+            builder.Services.AddDbContext<Contexto>(options => options.UseSqlite(ConStr));
+            builder.Services.AddScoped<ArticulosServices>();
 
             var app = builder.Build();
 
