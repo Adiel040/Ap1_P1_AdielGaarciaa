79e87ab6a8abb6010361124e0e527eb942ca6f52
Adiel040 <133602314+Adiel040@users.noreply.github.com>
2024-06-11 18:05:09 -0400

    Modificando Modelo

--- Ap1_P1_AdielGaarciaa/Models/Articulos.cs
+++ Ap1_P1_AdielGaarciaa/Models/Articulos.cs
@@ -6,14 +6,19 @@ namespace Ap1_P1_AdielGaarciaa.Models
     {
 
         //Articulo id, Descripcion , costo , ganancia , precio
-
         [Key]
         public int ArticuloId { get; set; }
         [Required(ErrorMessage = "El campo Descripci&oacute;n es obligatorio ")]
         public string? Descripcion { get; set; }
-        public decimal costo { get; set; }
-        public decimal ganancia { get; set; }
-        public decimal precio { get; set; }
+        [Required(ErrorMessage = "El campo Costo es obligatorio ")]
+
+        public decimal Costo { get; set; }
+        [Required(ErrorMessage = "El campo Ganancia es obligatorio ")]
+
+        public decimal Ganancia { get; set; }
+        [Required(ErrorMessage = "El campo Precio es obligatorio ")]
+
+        public decimal Precio { get; set; }
 
 
 
